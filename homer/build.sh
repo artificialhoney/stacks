b378a4db5baaee99896ebee1fef22539b10cd7f1
Sebastian Krüger <sk@honeymachine.io>
2024-08-04 09:43:24 +0200

    fix: build.sh

--- homer/build.sh
+++ homer/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker-compose up -d
+rm -rf ./dist
 wget --mirror -p --convert-links -P ./dist http://localhost:8080
+wget http://localhost:8080/sw.js -O ./dist/localhost:8080/sw.js
 cp -rf ./theme/assets/* ./dist/localhost:8080/assets/
-docker-compose stop
-
-rsync -avzhe ssh ./dist/localhost\:8080/* u71891509@home450438325.1and1-data.host:/kunden/homepages/7/d450438325/htdocs/sites/hi.honeymachine.io
+cp -f ./theme/LICENSE ./dist/localhost:8080/assets/
