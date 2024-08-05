10a4d4a2eb405b6ae97c863e79ed83ba4cf5c5a5
Sebastian Krüger <sk@honeymachine.io>
2024-08-05 02:23:15 +0200

    fix: remove submodule

--- homer/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ./dist
-wget --mirror -p --convert-links -P ./dist http://localhost:8080
-wget http://localhost:8080/sw.js -O ./dist/localhost:8080/sw.js
-cp -rf ./theme/assets/* ./dist/localhost:8080/assets/
-cp -f ./theme/LICENSE ./dist/localhost:8080/assets/
